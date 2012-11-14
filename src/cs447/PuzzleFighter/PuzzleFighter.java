f09125712535caeea2293dbddddf11d21b14b1e8
Kyle Siehl <krsiehl@gmail.com>
2012-11-14 12:13:52 -0800

    Initial commit

---
+++ src/cs447/PuzzleFighter/PuzzleFighter.java
@@ -0,0 +1,12 @@
+package cs447.PuzzleFighter;
+
+public class PuzzleFighter {
+
+	/**
+	 * @param args Ignored
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
