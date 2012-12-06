df4283b8175dad259b68b55ebcaca9e64205bdd3
ryantlk <ryantlk@ryantlk>
2012-12-05 21:39:53 -0800

    added diamond class have not implemented anything with the class yet.

---
+++ src/cs447/PuzzleFighter/Diamond.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs447.PuzzleFighter;
+
+/**
+ *
+ * @author ryantlk
+ */
+public class Diamond {
+	
+}
