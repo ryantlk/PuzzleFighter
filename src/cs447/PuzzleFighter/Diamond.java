bef7b7ad8d49fb625279e384167fd8b70cadfa98
ryantlk <ryantlk@ryantlk>
2012-12-10 17:49:05 -0800

    implemented diamond gem. currently extends color gem and every diamond is colored RED.

--- src/cs447/PuzzleFighter/Diamond.java
+++ src/cs447/PuzzleFighter/Diamond.java
@@ -4,10 +4,69 @@
  */
 package cs447.PuzzleFighter;
 
+import jig.engine.util.Vector2D;
+
 /**
  *
  * @author ryantlk
  */
-public class Diamond {
+public class Diamond extends ColoredGem{
+
+	public Diamond(PlayField pf, Vector2D pos, Color color){
+		super(pf, pos, Color.RED, "Diamond");
+	}
+	
+	@Override
+	public int endTurn() {
+		int crashCount = 0;
+		pf.clear(pos);
+		Gem g;
+		g = pf.ref(pos.translate(PlayField.DOWN));
+		if(!(g instanceof WallGem)){
+			Color c = ((ColoredGem)g).getColor();
+			System.out.println(c);
+			for(int x = 0; x < pf.getWidth(); x++){
+				for(int y = 0; y < pf.getHeight(); y++){
+					ColoredGem h = (ColoredGem)pf.ref(new Vector2D(x, y));
+					if(h != null && h.getColor() == c){
+						crashCount += h.crash(c, false);
+					}
+				}
+			}
+		}else{
+			return 5;
+		}
+		return crashCount;
+	}
+
+	@Override
+	public int crash(Color color, boolean initialCrash) {
+		int crashCount = 1;
+
+		pf.clear(pos);
+
+		Gem g;
+		g = pf.ref(pos.translate(PlayField.UP));
+		if (g != null) {
+			crashCount += g.crash(color, false);
+		}
+
+		g = pf.ref(pos.translate(PlayField.DOWN));
+		if (g != null) {
+			crashCount += g.crash(color, false);
+		}
+
+		g = pf.ref(pos.translate(PlayField.LEFT));
+		if (g != null) {
+			crashCount += g.crash(color, false);
+		}
+
+		g = pf.ref(pos.translate(PlayField.RIGHT));
+		if (g != null) {
+			crashCount += g.crash(color, false);
+		}
+
+		return crashCount;
+	}
 	
 }
