063f3db514210a4869f0fb82b97657fd751af95a
ryantlk <ryantlk@ryantlk>
2012-12-12 01:08:48 -0800

    Made changes to implement Networking. Some minor commented out lines to make networking work.

--- src/cs447/PuzzleFighter/PuzzleFighter.java
+++ src/cs447/PuzzleFighter/PuzzleFighter.java
@@ -60,7 +60,7 @@ public class PuzzleFighter extends StaticScreenGame {
 		boolean right1 = keyboard.isPressed(KeyEvent.VK_D);
 		boolean ccw1 = keyboard.isPressed(KeyEvent.VK_Q);
 		boolean cw1 = keyboard.isPressed(KeyEvent.VK_E);
-		/*pfRight.garbage +=*/ pfLeft.update(deltaMs, down1, left1, right1, ccw1, cw1);
+		pfRight.garbage += pfLeft.update(deltaMs, down1, left1, right1, ccw1, cw1);
 
 		boolean down2 = keyboard.isPressed(KeyEvent.VK_K);
 		boolean left2 = keyboard.isPressed(KeyEvent.VK_J);
